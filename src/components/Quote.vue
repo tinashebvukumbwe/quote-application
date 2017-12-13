b4d9795fb378e5690c0d81e85cb4f4fb2740f133
tinashebvukumbwe <trbvukumbwe1@gmail.com>
2017-12-13 15:15:15 +0200

    quote app

---
+++ src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+
+</script>
+
+
+
+
+
+<style>
+  .panel-body{
+    font-family:'Arizonia','cursize';
+    font-size:24px;
+    color:gray;
+  }
+  .quote {
+    cursor:pointer;
+
+  }
+  .quote:hover{
+    background-color:#ffe2e2;
+  }
+
+</style>
